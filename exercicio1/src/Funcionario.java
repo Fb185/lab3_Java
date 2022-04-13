065af7a8cfc2b3ee39472e73d9e0774720ede0b5
Fb185 <fredbobonejr@gmail.com>
2022-04-13 19:46:09 +0100

    thread daemon

--- exercicio1/src/Funcionario.java
+++ exercicio1/src/Funcionario.java
@@ -3,104 +3,152 @@ import java.util.TimerTask;
 
 // uncomment the exercise you want to run
 
-//exercicio 1
+// exercicio 1
 // public class funcionario extends Thread {
-//     public void run() {
-//         trabalhar();
-//     }
-
-//     public void trabalhar() {
-//         System.out.println(nome + " - Comecei");
-//         while (pecasProduzidas < TOTAL_PECAS) {
-//             try {
-
-//                 Thread.sleep(delay);
-//                 System.out.println(nome + ": " + ++pecasProduzidas);
-//             } catch (InterruptedException ex) {
-//                 System.out.println("ex");
-//             }
-//         }
-//         System.out.println(nome + " - terminei.");
-//     }
-
-//     String nome;
-//     int delay;
-//     int pecasProduzidas = 0;
-//     static int TOTAL_PECAS = 10;
-
-//     // constructor
-//     public funcionario(String nome, int delay) {
-//         this.nome = nome;
-//         this.delay = delay;
-//     }
-
-//     public static void main(String[] args) {
-//         funcionario manuel = new funcionario("Manuel", 3);
-//         funcionario pedro = new funcionario("\tPedro", 5);
-
-//         manuel.start();
-//         pedro.start();
-//         System.out.println("Main Termindado");
-//     }
+// public void run() {
+// trabalhar();
 // }
 
-//exercicio 2
+// public void trabalhar() {
+// System.out.println(nome + " - Comecei");
+// while (pecasProduzidas < TOTAL_PECAS) {
+// try {
+
+// Thread.sleep(delay);
+// System.out.println(nome + ": " + ++pecasProduzidas);
+// } catch (InterruptedException ex) {
+// System.out.println("ex");
+// }
+// }
+// System.out.println(nome + " - terminei.");
+// }
+
+// String nome;
+// int delay;
+// int pecasProduzidas = 0;
+// static int TOTAL_PECAS = 10;
+
+// // constructor
+// public funcionario(String nome, int delay) {
+// this.nome = nome;
+// this.delay = delay;
+// }
+
+// public static void main(String[] args) {
+// funcionario manuel = new funcionario("Manuel", 3);
+// funcionario pedro = new funcionario("\tPedro", 5);
+
+// manuel.start();
+// pedro.start();
+// System.out.println("Main Termindado");
+// }
+// }
+
+// exercicio 2
 // public class funcionario implements Runnable {
-//     public void run() {
-//         trabalhar();
-//     }
+// public void run() {
+// trabalhar();
+// }
 
-//     public void trabalhar() {
-//         System.out.println(nome + " - Comecei");
-//         while (pecasProduzidas < TOTAL_PECAS) {
-//             try {
+// public void trabalhar() {
+// System.out.println(nome + " - Comecei");
+// while (pecasProduzidas < TOTAL_PECAS) {
+// try {
 
-//                 Thread.sleep(delay);
-//                 System.out.println(nome + ": " + ++pecasProduzidas);
-//             } catch (InterruptedException ex) {
-//                 System.out.println("ex");
-//             }
-//         }
-//         System.out.println(nome + " - terminei.");
-//     }
+// Thread.sleep(delay);
+// System.out.println(nome + ": " + ++pecasProduzidas);
+// } catch (InterruptedException ex) {
+// System.out.println("ex");
+// }
+// }
+// System.out.println(nome + " - terminei.");
+// }
 
-//     String nome;
-//     int delay;
-//     int pecasProduzidas = 0;
-//     static int TOTAL_PECAS = 10;
+// String nome;
+// int delay;
+// int pecasProduzidas = 0;
+// static int TOTAL_PECAS = 10;
 
-//     // constructor
-//     public funcionario(String nome, int delay) {
-//         this.nome = nome;
-//         this.delay = delay;
-//     }
+// // constructor
+// public funcionario(String nome, int delay) {
+// this.nome = nome;
+// this.delay = delay;
+// }
 
-//     public static void main(String[] args) {
-//         funcionario manuel = new funcionario("Manuel", 3);
-//         funcionario pedro = new funcionario("\tPedro", 5);
+// public static void main(String[] args) {
+// funcionario manuel = new funcionario("Manuel", 3);
+// funcionario pedro = new funcionario("\tPedro", 5);
 
-//         Thread manuelthread = new Thread(manuel);
-//         Thread pedrothread = new Thread(pedro);
-//         manuelthread.start();
-//         pedrothread.start();
-//         System.out.println("Main Termindado");
-//     }
+// Thread manuelthread = new Thread(manuel);
+// Thread pedrothread = new Thread(pedro);
+// manuelthread.start();
+// pedrothread.start();
+// System.out.println("Main Termindado");
+// }
 // }
 
 // exercicio 3
-public class Funcionario {
+// public class Funcionario {
 
-    // constructor
-    public Funcionario(String nome, int delay) {
-        this.nome = nome;
-        this.delay = delay;
+// // constructor
+// public Funcionario(String nome, int delay) {
+// this.nome = nome;
+// this.delay = delay;
+// trabalhar();
+// }
+
+// String nome;
+// int delay;
+// int pecasProduzidas = 0;
+// static int TOTAL_PECAS = 10;
+
+// public void trabalhar() {
+// System.out.println(nome + " - Comecei");
+// while (pecasProduzidas < TOTAL_PECAS) {
+// try {
+
+// Thread.sleep(delay);
+// System.out.println(nome + ": " + ++pecasProduzidas);
+// } catch (InterruptedException ex) {
+// System.out.println("ex");
+// }
+// }
+// System.out.println(nome + " - terminei.");
+// }
+
+// public static void main(String[] args) {
+
+// Timer timer = new Timer();
+// TimerTask taskManuel = new TimerTask() {
+// public void run() {
+// Funcionario manuel = new Funcionario("Manuel", 3);
+// System.out.println("task is complete");
+
+// }
+// };
+// TimerTask taskPedro = new TimerTask() {
+// public void run() {
+// Funcionario pedro = new Funcionario("Pedro", 3);
+// System.out.println("task is complete");
+// timer.cancel();
+
+// }
+
+// };
+// timer.scheduleAtFixedRate(taskManuel, 3, 100);
+// timer.scheduleAtFixedRate(taskPedro, 5, 100);
+
+// }
+// }
+
+// exercicio 4
+import java.util.concurrent.TimeUnit;
+
+public class Funcionario extends Thread {
+
+    public void run() {
         trabalhar();
     }
-
-    String nome;
-    int delay;
-    int pecasProduzidas = 0;
-    static int TOTAL_PECAS = 10;
 
     public void trabalhar() {
         System.out.println(nome + " - Comecei");
@@ -116,27 +164,35 @@ public class Funcionario {
         System.out.println(nome + " - terminei.");
     }
 
+    String nome;
+    int delay;
+    int pecasProduzidas = 0;
+    static long TOTAL_PECAS = 1000;
+
+    // constructor
+    public Funcionario(String nome, int delay) {
+        this.nome = nome;
+        this.delay = delay;
+        trabalhar();
+    }
+
     public static void main(String[] args) {
+        Funcionario manuel = new Funcionario("Manuel", 3);
+        Funcionario pedro = new Funcionario("Pedro", 5);
 
-        Timer timer = new Timer();
-        TimerTask taskManuel = new TimerTask() {
-            public void run() {
-                Funcionario manuel = new Funcionario("Manuel", 3);
-                System.out.println("task is complete");
+        Thread manuelthread = new Thread(manuel);
+        manuelthread.setDaemon(true);
 
-            }
-        };
-        TimerTask taskPedro = new TimerTask() {
-            public void run() {
-                Funcionario pedro = new Funcionario("Pedro", 3);
-                System.out.println("task is complete");
-                timer.cancel();
+        Thread pedrothread = new Thread(pedro);
+        pedrothread.setDaemon(true);
 
-            }
+        long startTime = System.nanoTime();
+        manuelthread.start();
+        pedrothread.start();
+        long endTime = System.nanoTime();
 
-        };
-        timer.scheduleAtFixedRate(taskManuel, 3, 100);
-        timer.scheduleAtFixedRate(taskPedro, 5, 100);
-
+        System.out.println("Main Termindado");
+        long timeElapsed = endTime - startTime;
+        System.out.println("Execution time : " + timeElapsed);
     }
 }
